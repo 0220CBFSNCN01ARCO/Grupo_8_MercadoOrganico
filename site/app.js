dbe369dea2cbd10c7ffdc6ff5368f49c5d68ae11
MartinG94 <guillenmartin94@gmail.com>
2020-05-26 18:25:44 -0300

    Nueva variable para el manejo de archivos locales

--- site/app.js
+++ site/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override');
+global.__basedir = __dirname
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
