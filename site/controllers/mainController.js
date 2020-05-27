5fbd5ae065cd722a43d684fcaafdb2642a770a6b
MartinG94 <guillenmartin94@gmail.com>
2020-05-27 17:20:30 -0300

    Comentario

--- site/controllers/mainController.js
+++ site/controllers/mainController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+/* MAIN CONTROLLER */
 const controller = {
     root: (req, res, next) => {
         res.render('index', {title: 'Express'});
