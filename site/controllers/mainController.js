9cd23d2038be1d57dbc7267a5ee016034f366f77
MartinG94 <guillenmartin94@gmail.com>
2020-05-27 14:09:36 -0300

    Creado el metodo root del mainController

---
+++ site/controllers/mainController.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const controller = {
+    root: (req, res, next) => {
+        res.render('index', {title: 'Express'});
+    }
+};
+
+module.exports = controller;
