923f73fa0395f72fdd40c54b62cd53a2e0e99bfa
MartinG94 <guillenmartin94@gmail.com>
2020-05-27 23:20:44 -0300

    Creado el controller para los productos

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -4,11 +4,11 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-/* MAIN CONTROLLER */
+/* PRODUCTS CONTROLLER */
 const productsController = {
     root: (req, res, next) => {
         res.render('products', {title: 'Productos', listadoProductos: products});
     },
 };
 
-module.exports = controller;
+module.exports = productsController;
