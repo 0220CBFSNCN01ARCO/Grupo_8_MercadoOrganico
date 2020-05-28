fd3c020a9c8b062ce1d4c09609f1ce7b10dcd58a
MartinG94 <guillenmartin94@gmail.com>
2020-05-27 22:51:41 -0300

    Se agregó el titulo

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -1,8 +1,7 @@
 let usersController = {
-    'login': function (req, res){
-        res.render('login')
+    login: function (req, res){
+        res.render('login', {title: 'Usuarios'});
     },
-    
 };
 
 module.exports =  usersController;
