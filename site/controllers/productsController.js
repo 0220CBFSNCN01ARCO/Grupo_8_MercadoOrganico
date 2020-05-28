0088b0685a1a3dc962b60379f11efe82aacb0536
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 15:39:23 -0300

    Cambios

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -28,7 +28,7 @@ const productsController = {
         const nuevoID = cantidadProductos + 1;
         const nuevoProducto = {
             id: nuevoID,
-            name: body.nombre,
+            name: body.nombreProducto,
             brand: body.marca,
             category: body.categoria,
             discount: 0,
