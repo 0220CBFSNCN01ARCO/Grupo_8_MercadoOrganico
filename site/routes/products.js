f171b1237e8091e9212c6130b1c55a40cb267da5
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 15:13:36 -0300

    Se crea la ruta POST para registrar un producto

--- site/routes/products.js
+++ site/routes/products.js
@@ -5,5 +5,6 @@ var productsController = require('../controllers/productsController')
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
 router.get('/add', productsController.agregarProducto);
+router.post('/add', productsController.registrarProducto);
 
 module.exports = router;
