82b8d1982555b4638f429de9d1966f154d733ec0
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 12:45:01 -0300

    Nueva vista y rutas

--- site/routes/users.js
+++ site/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var usersController = require('../controllers/usersController')
 
 /* GET users listing. */
+router.get('/', usersController.root);
 router.get('/login', usersController.login);
 
 module.exports = router;
