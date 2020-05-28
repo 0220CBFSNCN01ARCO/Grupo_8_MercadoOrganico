5a57c12b08d604b22a63eca0086b08d39340e8b7
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 15:20:00 -0300

    Se configura Multer

--- site/routes/products.js
+++ site/routes/products.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var productsController = require('../controllers/productsController')
+var productsController = require('../controllers/productsController');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/usersAvatars');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
 
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
