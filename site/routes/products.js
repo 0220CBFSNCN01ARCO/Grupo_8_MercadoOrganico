f889064d01c89bd7f5c3e1001f6d5408ae0b0ca7
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 15:21:01 -0300

    Se implementa el middleware de multer

--- site/routes/products.js
+++ site/routes/products.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
-router.get('/add', productsController.agregarProducto);
+router.get('/add', upload.single('imagen-producto'), productsController.agregarProducto);
 router.post('/add', productsController.registrarProducto);
 
 module.exports = router;
