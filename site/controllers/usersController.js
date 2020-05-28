29395ed0639fae1b0fd38fbfd6f372d32c22ba28
MartinG94 <guillenmartin94@gmail.com>
2020-05-28 12:44:34 -0300

    Cambios en el userController

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -5,8 +5,11 @@ const path = require('path');
 // const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
+    root: (req, res) => {
+        res.render('usuario', {title: 'Usuario'});
+    },
     login: (req, res) => {
-        res.render('login', {title: 'Usuarios'});
+        res.render('login', {title: 'Login'});
     },
     register: (req, res) => {
 
