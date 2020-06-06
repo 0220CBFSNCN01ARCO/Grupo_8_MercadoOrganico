ee40f499e6185b04ec1a15586f8207c0a144fa1e
MartinG94 <guillenmartin94@gmail.com>
2020-06-06 19:54:08 -0300

    Format

--- site/routes/products.js
+++ site/routes/products.js
@@ -17,9 +17,12 @@ const upload = multer({ storage: storage });
 
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
+
 router.get('/add', productsController.agregarProducto);
 router.post('/add', upload.single('imagen-producto'), productsController.registrarProducto);
+
 router.get('/edit/:idProduct', productsController.editarProducto);
+
 router.get('/carrito', productsController.carrito);
 
 /* COMENTARIO */
