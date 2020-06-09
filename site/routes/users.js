d805cd063bd646ee7ea729737c351b6a4d119228
MartinG94 <guillenmartin94@gmail.com>
2020-06-09 18:13:09 -0300

    La carpeta de avatars ahora está dentro de images

--- site/routes/users.js
+++ site/routes/users.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/avatarUsuarios')
+      cb(null, 'public/music/avatarUsuarios')
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + '.png')
