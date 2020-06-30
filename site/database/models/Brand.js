942acd301172fa9d307b4ac73c15ff75f6e5983f
valentinabreda <valentinabreda.s@gmail.com>
2020-06-30 17:34:37 -0300

    brand model

---
+++ site/database/models/Brand.js
@@ -0,0 +1,20 @@
+dule.exports = (sequelize, DataTypes) => {
+    const alias = 'Brand';
+    const columnas = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoincrement: true,
+        },
+        brands_names: {
+            type: DataTypes.STRING
+        }
+    };
+    const config = {
+        tableName: 'brands', //nombre de la tabla
+        timestamps: false
+    };
+
+    const Brand = sequelize.define(alias, columnas, config);
+    return Brand;
+}
