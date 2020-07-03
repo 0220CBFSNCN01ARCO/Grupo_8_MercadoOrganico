1d8424a90baf68f89ef675e53d37ffd30ffebcd2
MartinG94 <guillenmartin94@gmail.com>
2020-07-03 13:32:14 -0300

    se elimina el metodo root de users

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -7,9 +7,6 @@ const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
-    root: (req, res) => {
-        res.render('usuario', {title: 'Usuario'});
-    },
     login: (req, res) => {
         res.render('login', {
             title: 'Login',
