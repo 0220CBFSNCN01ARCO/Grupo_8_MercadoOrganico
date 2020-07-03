1c1838fffdf369b90940e8ddabad8906a3f38272
MartinG94 <guillenmartin94@gmail.com>
2020-07-03 13:05:14 -0300

    Renombrando métodos y retocando multer

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var usersController = require('../controllers/usersController');
+const usersController = require('../controllers/usersController');
 const multer = require('multer');
+const path = require('path');
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images/avatarUsuarios')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + '.png')
-    }
-  });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/image/usersAvatars');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 
-var upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 /* GET users listing. */
 router.get('/', usersController.root);
 
-router.get('/login', usersController.login);
+router.get('/register', usersController.formRegister);
+router.post('/register', upload.single('avatar'), usersController.register);
 
-router.get('/register', usersController.register);
-router.post('/register', usersController.registrarCuenta);
+router.get('/login', usersController.login);
 
 router.get('/card', usersController.card);
 
