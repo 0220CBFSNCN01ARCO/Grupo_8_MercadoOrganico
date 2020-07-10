d213146b01d0d340e94404a5888003081a06b887
MartinG94 <guillenmartin94@gmail.com>
2020-07-10 17:26:51 -0300

    Se crea la ruta post para logearse

--- site/routes/users.js
+++ site/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const usersController = require('../controllers/usersController');
 const multer = require('multer');
 const path = require('path');
+const {check, validationResult, body} = require('express-validator');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,6 +23,10 @@ router.get('/register', usersController.formRegister);
 router.post('/register', upload.single('avatar'), usersController.register);
 
 router.get('/login', usersController.login);
+router.post('/login', [
+  check('email').isEmail().withMessage('Email inválido'),
+  check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
+], usersController.processLogin);
 
 router.get('/card', usersController.card);
 
