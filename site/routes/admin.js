e0abf68a842dfe58e5a16a39fbe9f80559930d12
MartinG94 <guillenmartin94@gmail.com>
2020-07-11 19:11:31 -0300

    Ya no se requiere adminMiddleware en la ruta de admin

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const adminController = require('../controllers/adminController');
-const adminMiddleware = require('../middlewares/adminMiddleware');
 
-router.get('/', adminMiddleware, adminController.root);
+router.get('/', adminController.root);
 // router.get('/products', adminController.productList);
 // router.get('/users', adminController.userList);
 
