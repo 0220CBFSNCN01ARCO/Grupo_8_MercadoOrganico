b1e92d4319658bd82ca47ef73f0b4d59334d267f
valentinabreda <valentinabreda.s@gmail.com>
2020-07-11 18:36:20 -0300

    se borro consolelog de middleware

--- site/middlewares/adminMiddleware.js
+++ site/middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 function adminMiddleware(req, res, next) {
-    console.log(req.session);
+    
     if(req.session.usuarioLogeado == undefined || !req.session.usuarioLogeado.admin) {
         return res.send('Acceso Denegado');
     };
