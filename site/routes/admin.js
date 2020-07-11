8ee4bfab05b9833fa8375e03421804c325d17ae7
valentinabreda <valentinabreda.s@gmail.com>
2020-07-11 19:35:05 -0300

    cambio insignificante

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const adminController = require('../controllers/adminController')
 
 router.get('/', adminController.root);
 router.get('/products', adminController.productList);
