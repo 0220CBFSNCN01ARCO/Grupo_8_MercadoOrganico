6564f5b2af880e3cadc7f89a1cabc25812917051
MartinG94 <guillenmartin94@gmail.com>
2020-07-11 17:58:51 -0300

    Ruta get para /admin

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const adminController = require('../controllers/adminController');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 
+router.get('/', adminMiddleware, adminController.root);
+
 module.exports = router;
