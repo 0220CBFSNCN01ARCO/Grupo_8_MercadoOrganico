da0ac81d7639c84f30a1c1f636818d4ac5a0b052
MartinG94 <guillenmartin94@gmail.com>
2020-07-11 17:50:21 -0300

    Se requiere el middleware para controlar que el usuario sea admin en la ruta correspondiente

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const adminController = require('../controllers/adminController');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
 module.exports = router;
