bf61fc320c9751f92e12516a2544f1404b57fbe0
MartinG94 <guillenmartin94@gmail.com>
2020-07-17 16:35:45 -0300

    Se eliminan las rutas correspondientes

--- site/routes/products.js
+++ site/routes/products.js
@@ -1,19 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var productsController = require('../controllers/productsController');
-const path = require('path');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images/products');
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-  });
-
-const upload = multer({ storage: storage });
 
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
