d0061479760c450f66c7369c5a8fc8fc850333e3
MartinG94 <guillenmartin94@gmail.com>
2020-07-17 16:09:56 -0300

    Se crean las ruta correspondientes de admin para editar y eliminar productos

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -3,7 +3,15 @@ var router = express.Router();
 const adminController = require('../controllers/adminController')
 
 router.get('/', adminController.root);
+
 router.get('/products', adminController.productList);
+
+router.get('/products/edit/:id', adminController.editProduct);
+router.put('/products/edit/:id', adminController.updateProduct);
+
+router.get('/products/delete/:id', adminController.confirmDeleteProduct);
+router.delete('/products/delete/:id', adminController.deleteProduct);
+
 router.get('/users', adminController.userList);
 
 module.exports = router;
