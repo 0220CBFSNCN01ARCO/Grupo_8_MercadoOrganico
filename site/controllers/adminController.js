5b4a31940b331333cfaad9b9ea2f36118f8e6e53
MartinG94 <guillenmartin94@gmail.com>
2020-07-17 16:09:38 -0300

    Se crean los métodos necesarios para editar y eliminar productos

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
@@ -23,7 +24,54 @@ const adminController = {
             title: 'Users Editor',
             listadoUsuarios: users
         });
-    }
+    },
+    editProduct: (req, res) => {
+        let idProducto = req.params.id;
+        let productoAEditar = products.find( producto => {
+            return producto.id == idProducto;
+        });
+        res.render('admin/adminProductEdit', {
+            title: productoAEditar.name,
+            producto: productoAEditar,
+            user: req.session.usuarioLogeado
+        });
+    },
+    updateProduct: (req, res) => {
+        const idProducto = req.params.id;
+        console.log(idProducto)
+        const body = req.body;
+        products.map( producto => {
+            if(producto.id == idProducto){
+                producto.name = body.nombreProducto,
+                producto.brand = body.marca,
+                producto.category = body.categoria,
+                producto.description = body.descripcion,
+                producto.price = body.precio,
+                producto.image = req.file.filename
+            };
+        });
+        fs.writeFileSync(productsFilePath, JSON.stringify(products));
+        res.redirect('/admin/products');
+    },
+    confirmDeleteProduct: (req, res) => {
+        let productoBuscado = products.find( (elemento) => {
+            return elemento.id == req.params.id;
+        });
+        res.render('admin/adminProductDelete', {
+            title: 'Eliminar producto',
+            product: productoBuscado,
+            user: req.session.usuarioLogeado
+        });
+    },
+    deleteProduct: (req, res) => {
+        let idProductoBuscado = req.params.id;
+        let productosSinProductoBuscado = products.filter( product =>{
+            return product.id != idProductoBuscado;
+        });
+        let nuevosProductos = JSON.stringify(productosSinProductoBuscado,'utf-8');
+        fs.writeFileSync(productsFilePath, nuevosProductos);
+        res.redirect("/admin/products")
+    },
 };
 
 module.exports = adminController;
