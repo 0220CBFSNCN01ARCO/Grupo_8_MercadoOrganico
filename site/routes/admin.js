4a1721753fa743ca3c489fa91f9a7ec12c2ed294
MartinG94 <guillenmartin94@gmail.com>
2020-07-17 16:35:24 -0300

    Se crean todas las rutas necesarias para crear, editar y eliminar productos

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const adminController = require('../controllers/adminController')
+const adminController = require('../controllers/adminController');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/products');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+const upload = multer({ storage: storage });
 
 router.get('/', adminController.root);
 
 router.get('/products', adminController.productList);
 
+router.get('/products/create', adminController.createProduct);
+router.post('/products/create', upload.single('imagen-producto'), adminController.registerProduct);
+
 router.get('/products/edit/:id', adminController.editProduct);
-router.put('/products/edit/:id', adminController.updateProduct);
+router.put('/products/edit/:id', upload.single('imagen-producto'), adminController.updateProduct);
 
 router.get('/products/delete/:id', adminController.confirmDeleteProduct);
 router.delete('/products/delete/:id', adminController.deleteProduct);
