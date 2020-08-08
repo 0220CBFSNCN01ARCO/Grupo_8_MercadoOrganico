e5c100a1386e2386e5a39bb57cb64a17f2553d6f
MartinG94 <guillenmartin94@gmail.com>
2020-08-08 17:11:45 -0300

    Middleware emailExistente

--- site/middlewares/usersMiddleware.js
+++ site/middlewares/usersMiddleware.js
@@ -1,8 +1,31 @@
-function usersMiddleware(req, res, next) {
+const models = require('../database/models');
+
+function loginMiddleware(req, res, next) {
     if( req.session.usuarioLogeado != undefined ) {
         return res.redirect('/');
     };
     return next();
 };
 
-module.exports = usersMiddleware;
+function emailExistente(req, res, next){
+    models.User.findAll()
+        .then( (usuarios) => {
+            for(let usuario of usuarios){
+                if(usuario.email == req.body.email){
+                    res.render('register', {
+                        title: 'Register',
+                        errors: [
+                            {msg: 'Email ya existente'}
+                        ],
+                        user: req.session.usuarioLogeado
+                    });
+                    break;
+                };
+            };
+        })
+        .catch(error => {
+            return res.send('Ocurrió un error');
+        });
+};
+
+module.exports = {loginMiddleware, emailExistente};
