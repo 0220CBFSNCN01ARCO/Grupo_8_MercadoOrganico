8813d99bc64543776070a39155ac61cf6f65ebdf
MartinG94 <guillenmartin94@gmail.com>
2020-08-08 16:04:06 -0300

    Vuelve el adminMiddleware

--- site/middlewares/adminMiddleware.js
+++ site/middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 function adminMiddleware(req, res, next) {
-    if(req.session.usuarioLogeado == undefined || !req.session.usuarioLogeado.admin) {
+    if((req.session.usuarioLogeado == undefined) || (req.session.usuarioLogeado.id_type != 1)) {
         return res.send('Acceso Denegado');
     };
     return next();
