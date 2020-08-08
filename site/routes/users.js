cf3f4c4aa9351f0a509eed3b8f7a5bb1a46c9b20
MartinG94 <guillenmartin94@gmail.com>
2020-08-08 15:44:50 -0300

    Se eliminan las validaciones

--- site/routes/users.js
+++ site/routes/users.js
@@ -19,23 +19,24 @@ const upload = multer({ storage: storage });
 /* GET users listing. */
 
 router.get('/register', usersController.formRegister);
-router.post('/register', upload.single('avatar'), [
-  check('nombre').isLength({min: 1}).withMessage('Debe ingresar un nombre'),
-  check('email').isEmail().withMessage('El email debe ser un email válido'),
-  check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
-], usersController.register);
+router.post('/register', upload.single('avatar'), usersController.register);
+
+// [
+//   check('nombre').isLength({min: 1}).withMessage('Debe ingresar un nombre'),
+//   check('email').isEmail().withMessage('El email debe ser un email válido'),
+//   check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
+// ]
 
 router.get('/login', usersController.login);
-router.post('/login', [
-  check('email').isEmail().withMessage('Email inválido'),
-  check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
-], usersController.processLogin);
+router.post('/login', usersController.processLogin);
+
+// [
+//   check('email').isEmail().withMessage('Email inválido'),
+//   check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
+// ]
 
 router.get('/logout', usersController.logout);
 
 router.get('/card', usersController.card);
-
-// router.get('/login/crearcuenta', usersController.crearCuenta);
-// router.post('/login/crearcuenta', upload.single('avatar'), usersController.registrarCuenta);
 
 module.exports = router;
