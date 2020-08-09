bc1805937f077d731be6dc00cba3b1c6f58bb015
MartinG94 <guillenmartin94@gmail.com>
2020-08-09 15:23:21 -0300

    Ruta post para buscar productos

--- site/routes/products.js
+++ site/routes/products.js
@@ -5,6 +5,8 @@ var productsController = require('../controllers/productsController');
 router.get('/', productsController.root);
 router.get('/detail/:id', productsController.detallarProducto);
 
+router.post('/', productsController.buscar);
+
 /* COMENTARIO */
 
 module.exports = router;
