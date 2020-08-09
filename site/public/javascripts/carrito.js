db52e7e6dbd2c017f8e08897301a5c7644dca28f
MartinG94 <guillenmartin94@gmail.com>
2020-08-09 19:24:23 -0300

    Carrito de compras funcional

--- site/public/javascripts/carrito.js
+++ site/public/javascripts/carrito.js
@@ -4,8 +4,33 @@ class Carrito {
         e.preventDefault();
         if(e.target.classList.contains('agregar-carrito')){
             const producto = e.target.parentElement.parentElement;
-            // this.leerDatosProducto(producto);
-            console.log(producto);
+            this.leerDatosProducto(producto);
         }
-    }
+    };
+
+    leerDatosProducto(producto) {
+        const infoProducto = {
+            imagen: producto.querySelector('img').src,
+            titulo: producto.querySelector('h2').textContent,
+            precio: producto.querySelector('.precio span').textcContent,
+            id: producto.querySelector('#getId').getAttribute('data-id'),
+            cantidad: 1
+        };
+        this.insertarCarrito(infoProducto);
+    };
+
+    insertarCarrito(producto){
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+        <td>
+            <img src="${producto.imagen}" width=100>
+        </td>
+        <td>${producto.titulo}</td>
+        <td>${producto.precio}</td>
+        <td>
+            <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+        </td>
+        `;
+        listaProductos.appendChild(fila);
+    };
 };
